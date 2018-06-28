d07ff05d8d844c19e1794fa0c599d80bf82e52c9
shreyas2047 <shreyass.hf95@gmail.com>
2018-06-28 14:07:15 -0700

    BackEnd for the FaceRecognitionProject

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors');
+const db = require('knex')({
+    client: 'pg',
+    connection: {
+      host : '127.0.0.1',
+      user : 'postgres',
+      password : 'sama',
+      database : 'smartbrain'
+    }
+  });
+const signin = require('./controllers/signin');
+const register = require('./controllers/register');
+const profile = require('./controllers/profile');
+const image = require('./controllers/image');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(cors());
+
+
+
+app.post('/signin', (req, res) => signin.handleSignIn(req, res, db, bcrypt));
+app.post('/register',(req, res)=> register.handleRegister(req, res, db, bcrypt));
+app.get('/profile/:id', (req, res) => profile.handleProfileGet(req, res, db));
+app.put('/image', (req, res) => image.handleImage(req, res, db));
+
+app.listen(3000, () => {
+    console.log(`Server started on port 3000`);
+});
