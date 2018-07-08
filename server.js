7a4ba3c2112df1974bef8ff038bf2eed92e81555
shreyas2047 <shreyass.hf95@gmail.com>
2018-07-08 14:22:24 -0400

    added root route

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.use(bodyParser.json());
 app.use(cors());
 
 
-
+app.get('/', (req, res)=> res.send('It is working'));
 app.post('/signin', (req, res) => signin.handleSignIn(req, res, db, bcrypt));
 app.post('/register',(req, res)=> register.handleRegister(req, res, db, bcrypt));
 app.get('/profile/:id', (req, res) => profile.handleProfileGet(req, res, db));
